c1ad4c469d6e823b62846772da16e7fc2f86efcd
g-e-o-m-e-t-r-i-c <g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2022-02-20 12:09:19 +0000

    deploy: cfc9d08485effeb6560d730c28bc791281c2d290

---
+++ assets/LICENSE.html.154671ae.js
@@ -0,0 +1,1 @@
+const e={key:"v-008f5444",path:"/guide/LICENSE.html",title:"LICENSE",lang:"en-US",frontmatter:{title:"LICENSE"},excerpt:"",headers:[],git:{updatedTime:1645358926e3,contributors:[{name:"g-e-o-m-e-t-r-i-c",email:"79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com",commits:1}]},filePathRelative:"guide/LICENSE.md"};export{e as data};
--- assets/gt.html.2f722b05.js
+++
@@ -1,1 +0,0 @@
-const e={key:"v-1da06924",path:"/guide/more-adv/gt.html",title:"GT",lang:"en-US",frontmatter:{},excerpt:"",headers:[],git:{updatedTime:1645357771e3,contributors:[{name:"g-e-o-m-e-t-r-i-c",email:"79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com",commits:1}]},filePathRelative:"guide/more-adv/gt.md"};export{e as data};
