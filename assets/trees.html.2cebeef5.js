8295514e3c409ff2cfa04aed5fe7c72d9ea940d6
g-e-o-m-e-t-r-i-c <g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2022-02-20 06:38:47 +0000

    deploy: cd987e5ee942d36bb62aca0cd3f3ea528ef99162

---
+++ assets/trees.html.2cebeef5.js
@@ -0,0 +1,1 @@
+const e={key:"v-ca431568",path:"/guide/more-adv/trees.html",title:"Trees",lang:"en-US",frontmatter:{},excerpt:"",headers:[],git:{updatedTime:1645339095e3,contributors:[{name:"g-e-o-m-e-t-r-i-c",email:"79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com",commits:1}]},filePathRelative:"guide/more-adv/trees.md"};export{e as data};
