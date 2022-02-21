ad4bfe9913ab6b5add675d1fdb4d0dc059c954aa
DET171 <DET171@users.noreply.github.com>
2022-02-21 09:33:43 +0000

    deploy: 9fcf2d744a033828876cc9aca7dd67b335fe7066

---
+++ assets/LICENSE.html.323af2b9.js
@@ -0,0 +1,1 @@
+const e={key:"v-008f5444",path:"/guide/LICENSE.html",title:"LICENSE",lang:"en-US",frontmatter:{title:"LICENSE"},excerpt:"",headers:[],git:{updatedTime:164543597e4,contributors:[{name:"Dusty",email:"mrgeek484@gmail.com",commits:1}]},filePathRelative:"guide/LICENSE.md"};export{e as data};
