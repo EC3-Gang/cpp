41787624d38e1d1dbbf43562c6a355d4d918ea60
Canaris <mrgeek484@gmail.com>
2022-02-22 02:16:20 +0000

    a little theme

---
+++ docs/.vuepress/theme/Layout.vue
@@ -0,0 +1,17 @@
+<script setup>
+import ParentLayout from '@vuepress/theme-default/lib/client/layouts/Layout.vue'
+</script>
+
+<template>
+  <ParentLayout>
+    <template #page-bottom>
+      <div class="center">Made with ♥ and Vuepress</div>
+    </template>
+  </ParentLayout>
+</template>
+
+<style lang="css">
+.center {
+  text-align: center;
+}
+</style>
