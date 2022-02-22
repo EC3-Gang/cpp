56a7f471651fb8afc507d7ada8e0b0d402e5af64
Canaris <mrgeek484@gmail.com>
2022-02-22 02:29:40 +0000

    this might break

--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -10,6 +10,18 @@ import ParentLayout from '@vuepress/theme-default/lib/client/layouts/Layout.vue'
   </ParentLayout>
 </template>
 
+<script>
+import 'https://cdn.jsdelivr.net/npm/katex@0.15.2/dist/katex.min.css';
+import renderMathInElement from "https://cdn.jsdelivr.net/npm/katex@0.15.2/dist/contrib/auto-render.mjs";
+	
+export default {
+	name: 'theme',
+	mounted() {
+		renderMathInElement(document.body);
+	}
+}
+</script>
+
 <style lang="css">
 .center {
   text-align: center;
