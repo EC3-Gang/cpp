f8d2ec69f3b80b9b02cfc190931311358058d1db
Dusty <mrgeek484@gmail.com>
2022-02-27 11:11:53 +0800

    unbreake site

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,8 +4,6 @@ module.exports = {
 	theme: path.resolve(__dirname, './theme'),
 	sidebarDepth: 5,
 	base: process.env.BUILD_PATH,
-	templateDev: path.join(__dirname, 'template', 'index.html'),
-	templateBuild: path.join(__dirname, 'template', 'index.html'),
 	head: [
 		['link', { rel: 'icon', href: 'https://i.imgur.com/gh25FnY_d.png' }],
 		[
